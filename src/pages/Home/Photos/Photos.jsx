310a938f686584103d17e086eb7b3de0adc3b188
rabiulislam13579 <rabiul120158@gmail.com>
2023-05-21 20:26:01 +0600

    photos

---
+++ src/pages/Home/Photos/Photos.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import ShowPhoto from './ShowPhoto';
+
+const Photos = () => {
+    const [photos,setPhotos]= useState([])
+    useEffect(()=>{
+        fetch('https://toy-server-pi.vercel.app/toys')
+        .then(res=>res.json())
+        .then(data=>setPhotos(data))
+    },[])
+
+
+
+    return (
+        <div className='md:mx-48'>
+            <div>
+                <h2 className='text-center text-2xl text-purple-900 my-3 font-bold'>Photo Gallery</h2>
+            </div>
+            <div className='grid md:grid-cols-3'>
+            {
+                photos.slice(0,6).map(photo=><ShowPhoto
+                key={photo._id}
+                photo={photo}
+                ></ShowPhoto>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Photos;
